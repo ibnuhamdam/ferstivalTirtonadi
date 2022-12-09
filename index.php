5b1ecbfa9ec9c27c420a646413748f2a369f4bab
Ibnu hamdam <ibnu.hamdam16@gmail.com>
2022-12-09 19:54:44 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,366 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+  <meta charset="UTF-8" />
+  <title>Giner - Conference page</title>
+  <!-- =================== META =================== -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="format-detection" content="telephone=no" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <link rel="shortcut icon" href="assets/img/favicon.png" />
+  <!-- =================== STYLE =================== -->
+  <link rel="stylesheet" type="text/css" href="assets/css/slick.min.css" />
+  <link rel="stylesheet" href="assets/css/bootstrap-grid.css" />
+  <link href="https://use.fontawesome.com/releases/v5.10.1/css/all.css" rel="stylesheet" />
+  <link rel="stylesheet" href="assets/css/nice-select.css" />
+  <link rel="stylesheet" href="assets/css/style.css" />
+</head>
+
+<body id="conference-page" style="background-image: url(assets/img/conference_bg.svg)">
+  <!-- =============== PRELOADER =============== -->
+  <div class="page-preloader-cover">
+    <div class="cssload-loader">
+      <div class="cssload-inner cssload-one"></div>
+      <div class="cssload-inner cssload-two"></div>
+      <div class="cssload-inner cssload-three"></div>
+    </div>
+  </div>
+  <!-- ============== PRELOADER END ============== -->
+  <!-- ================= HEADER ================= -->
+  <header class="conference-header-fixed header-fixed">
+    <a href="#" class="nav-btn">
+      <span></span>
+      <span></span>
+      <span></span>
+    </a>
+    <div class="container">
+      <div class="row conference-header-row">
+        <div class="col-sm-3 col-lg-2 col-xl-3">
+          <a href="index.html" class="logo"><img src="assets/img/logo-bt.svg" alt="logo" /></a>
+        </div>
+        <div class="col-sm-9 col-lg-8 col-xl-6">
+          <nav class="nav-menu menu">
+            <ul class="nav-list">
+              <li class="dropdown active-page">
+                <a href="#">home </a>
+              </li>
+              <li><a href="#about">Tema</a></li>
+              <li><a href="#schedule">Bintang Tamu</a></li>
+              <li><a href="#news">Lomba</a></li>
+              <li><a href="#location">Lokasi</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
+  <!-- =============== HEADER END =============== -->
+
+  <!-- =========== S-CONFERENCE-SLIDER =========== -->
+  <section class="s-conference-slider">
+    <div class="conference-slider">
+      <div class="conference-slide" style="background-image: url(assets/img/background-1.png)">
+        <img class="conference-slide-tringle" src="assets/img/effect-tringle-slider.svg" alt="img" />
+        <img class="conference-slide-effect" src="assets/img/effect-slider-left.svg" alt="img" />
+        <div class="container">
+          <div style="width: 40%">
+            <img src="assets/img/logo-bt.png" width="80%" alt="" />
+          </div>
+          <div class="conference-slide-item">
+            <div class="date">17 - 18 Desember 2022</div>
+            <div class="conference-slider-title"></div>
+            <h2 class="title"><span>Festival Bendung Tirtonadi</span></h2>
+            <p>Di Pedestrian Bendung Tirtonadi Kota Surakarta</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- ========= S-CONFERENCE-SLIDER END ========= -->
+
+  <!-- =========== S-CONFERENCE-COUNTER =========== -->
+  <section id="about" class="s-conference-mission" style="background-image: url(assets/img/bg-about-home2.png)">
+    <div class="s-conference-counter">
+      <div class="container">
+        <div class="conference-counter-wrap">
+          <img class="conference-counter-effect-1" src="assets/img/counter-icon-1.svg" alt="img" />
+          <div class="conference-counter-cover">
+            <h4>Festival akan dimulai pada</h4>
+            <div id="clockdiv" class="clock-timer clock-timer-conference">
+              <div class="clock-item days-item">
+                <span class="days"></span>
+                <div class="smalltext">Hari</div>
+              </div>
+              <div class="clock-item hours-item">
+                <span class="hours"></span>
+                <div class="smalltext">Jam</div>
+              </div>
+              <div class="clock-item minutes-item">
+                <span class="minutes"></span>
+                <div class="smalltext">Menit</div>
+              </div>
+              <div class="clock-item seconds-item">
+                <span class="seconds"></span>
+                <div class="smalltext">Detik</div>
+              </div>
+            </div>
+          </div>
+          <img class="conference-counter-effect-2" src="assets/img/counter-icon-2.svg" alt="img" />
+        </div>
+      </div>
+    </div>
+    <div class="s-our-mission s-about-speaker">
+      <div class="container">
+        <h2 class="title-conference"><span>Tema Festival</span></h2>
+        <div class="row">
+          <div class="col-lg-6 our-mission-img">
+            <span>
+              <img src="assets/img/placeholder-all.png" data-src="assets/img/our-mission-2.svg" alt="" class="mission-img-effect-1 rx-lazy" />
+              <a href="assets/img/gall-dance-1.jpg" data-fancybox="Festival1">
+                <img class="mission-img rx-lazy" src="assets/img/placeholder-all.png" data-src="assets/img/poster-bt2.jpg" alt="img" />
+              </a>
+              <img src="assets/img/placeholder-all.png" data-src="assets/img/tringle-gray-little.svg" alt="" class="about-img-effect-2 rx-lazy" />
+            </span>
+          </div>
+          <div class="col-lg-6 our-mission-info">
+            <h4 style="text-transform: uppercase">masyarakat peduli, bengawan solo lestari, surakarta jaya</h4>
+            <div class="mission-info-text">
+              <p>
+                Dengan adanya masyarakat yang peduli akan pengelolaan sungai melalui upaya menjaga dan memelihara insfrastruktur yang ada, maka diharapkan Sungai Bengawan Solo yang ada di Kota Surakarta menjadi lestari dan bebas dari
+                bencana banjir sehingga dapat mendorong pertumbuhan ekonomi Kota Surakarta melalui wisata budaya, kuliner, dan kerajinan serta UMKM dan ekonomi kreatif lainnya
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- ========= S-CONFERENCE-COUNTER END ========= -->
+
+  <!-- ============ SPEAKER & SCHEDULE ============ -->
+  <section id="schedule" class="s-speakers-schedule">
+    <div class="container">
+      <h2 class="title-conference"><span>Bintang Tamu</span></h2>
+      <div class="speakers-timeline-cover">
+        <div class="speakers-timeline-item">
+          <div class="speakers-timeline-img">
+            <div class="our-speaker-item">
+              <a href="assets/img/gall-dance-1.jpg" data-fancybox="Festival1">
+                <img class="rx-lazy" src="assets/img/placeholder-all.png" data-src="assets/img/pecasndahe-new.png" alt="img" />
+              </a>
+            </div>
+          </div>
+          <div class="speakers-timeline-info">
+            <h3 class="title">PecasNdahe</h3>
+          </div>
+        </div>
+        <div class="speakers-timeline-item">
+          <div class="speakers-timeline-img">
+            <div class="our-speaker-item">
+              <img class="rx-lazy" src="assets/img/placeholder-all.png" data-src="assets/img/OnnyAndFriend.png" alt="img" />
+            </div>
+          </div>
+          <div class="speakers-timeline-info">
+            <h3 class="title">Oni n Friends</h3>
+          </div>
+        </div>
+        <div class="speakers-timeline-item">
+          <div class="speakers-timeline-img">
+            <div class="our-speaker-item">
+              <img class="rx-lazy" src="assets/img/placeholder-all.png" data-src="assets/img/ohmydrop.png" alt="img" />
+            </div>
+          </div>
+          <div class="speakers-timeline-info">
+            <h3 class="title">Oh My Drop!</h3>
+          </div>
+        </div>
+        <div class="speakers-timeline-item">
+          <div class="speakers-timeline-img">
+            <div class="our-speaker-item">
+              <img class="rx-lazy" src="assets/img/placeholder-all.png" data-src="assets/img/keroncongPDAM.png" alt="img" />
+            </div>
+          </div>
+          <div class="speakers-timeline-info">
+            <h3 class="title">Keroncong PDAM</h3>
+          </div>
+        </div>
+        <div class="speakers-timeline-item">
+          <div class="speakers-timeline-img">
+            <div class="our-speaker-item">
+              <img class="rx-lazy" src="assets/img/placeholder-all.png" data-src="assets/img/dimassamodra.png" alt="img" />
+            </div>
+          </div>
+          <div class="speakers-timeline-info">
+            <h3 class="title">Dimas Samodra</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- ========== SPEAKER & SCHEDULE END ========== -->
+
+  <!--================ CONFERENCE NEWS ================-->
+  <section id="news" class="s-conference-news" style="background: rgb(1, 45, 134)">
+    <div class="conference-news-container">
+      <h2 class="title-conference title-conference-white"><span>Daftar Perlombaan</span></h2>
+      <div class="conference-news-slider">
+        <div class="conference-news-slide">
+          <div class="conference-news-item">
+            <div class="conference-post-thumbnail">
+              <a href="conference-team.html"><img src="assets/img/lomba-dayung-750.png" alt="img" /></a>
+            </div>
+            <div class="date"><span>18</span>Des 2022</div>
+            <div class="conference-post-content">
+              <h4><a href="single-blog.html">Lomba Dayung</a></h4>
+
+              <p style="margin-bottom: 50px;">
+                Narahubung <br />
+                <a href="https://wa.me/6281931666217" target="_blank">Sabar Gorky 0819-3166-6217</a>
+              </p>
+
+              <a href="https://wa.me/6281931666217" target="__BLANK" class="btn"><span>daftar</span></a>
+              <a href="https://www.instagram.com/p/Cl3RdiSPg3I/?utm_source=ig_web_copy_link" target="__BLANK" class="btn"><span><i class="fab fa-instagram" style="color: white"></i></span></a>
+            </div>
+          </div>
+        </div>
+        <div class="conference-news-slide">
+          <div class="conference-news-item">
+            <div class="conference-post-thumbnail">
+              <a href="conference-team.html"><img src="assets/img/lomba-vlog.png" alt="img" /></a>
+            </div>
+            <div class="date"><span>15-22</span>Des 2022</div>
+            <div class="conference-post-content">
+              <h4><a href="single-blog.html">Lomba Vlog & Foto</a></h4>
+
+              <p style="margin-bottom: 50px;">
+                Narahubung <br />
+                <a href="https://wa.me/6281234559512" target="_blank">Laksita - 0812-3455-9512</a>
+              </p>
+
+              <a href="https://docs.google.com/forms/d/e/1FAIpQLSccKtvzUr7vmM9xi2M-AEKs_LQIp4I5hCa3BCprmdnLivWXFQ/viewform" target="__BLANK" class="btn"><span>daftar</span></a>
+              <a href="https://www.instagram.com/p/Cl57JKaPxZZ/?utm_source=ig_web_copy_link" target="__BLANK" class="btn"><span><i class="fab fa-instagram" style="color: white"></i></span></a>
+            </div>
+          </div>
+        </div>
+        <div class="conference-news-slide">
+          <div class="conference-news-item">
+            <div class="conference-post-thumbnail">
+              <a href="single-blog.html"><img src="assets/img/lomba-gambar.png" alt="img" /></a>
+            </div>
+            <div class="date"><span>18</span>Des 2022</div>
+            <div class="conference-post-content">
+              <h4><a href="single-blog.html">Lomba Menggambar</a></h4>
+              <p style="margin-bottom: 50px;">
+                Narahubung <br />
+                <a href="https://wa.me/6281234559512" target="_blank">Laksita - 0812-3455-9512</a>
+              </p>
+
+              <a href="https://docs.google.com/forms/d/e/1FAIpQLSccKtvzUr7vmM9xi2M-AEKs_LQIp4I5hCa3BCprmdnLivWXFQ/viewform" target="__BLANK" class="btn"><span>daftar</span></a>
+              <a href="https://www.instagram.com/p/Cl57JKaPxZZ/?utm_source=ig_web_copy_link" target="__BLANK" class="btn"><span><i class="fab fa-instagram" style="color: white"></i></span></a>
+            </div>
+          </div>
+        </div>
+        <div class="conference-news-slide">
+          <div class="conference-news-item">
+            <div class="conference-post-thumbnail">
+              <a href="single-blog.html"><img src="assets/img/fashion.png" alt="img" /></a>
+            </div>
+            <div class="date"><span>17</span>Des 2022</div>
+            <div class="conference-post-content">
+              <h4><a href="single-blog.html">Lomba Fashion Show</a></h4>
+              <p style="margin-bottom: 50px;">
+                Narahubung <br />
+                <a href="https://wa.me/6281234559512" target="_blank">Eva - 0823-2429-3514</a>
+              </p>
+
+              <a href="https://docs.google.com/forms/d/e/1FAIpQLSccKtvzUr7vmM9xi2M-AEKs_LQIp4I5hCa3BCprmdnLivWXFQ/viewform" target="__BLANK" class="btn"><span>daftar</span></a>
+              <a href="https://www.instagram.com/p/Cl57JKaPxZZ/?utm_source=ig_web_copy_link" target="__BLANK" class="btn"><span><i class="fab fa-instagram" style="color: white"></i></span></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--============== CONFERENCE NEWS END ==============-->
+
+  <!-- =============== CONFERENCE-MAP =============== -->
+  <section id="location" class="conference-map">
+    <div class="container">
+      <h2 class="title-conference"><span>Lokasi Festival</span></h2>
+      <div class="row">
+        <div class="col-lg-5 conference-map-info">
+          <ul class="mission-meta">
+            <li><i aria-hidden="true" class="fas fa-calendar-alt"></i>17-18 Des 2022</li>
+            <li><i class="far fa-clock"></i>07:00 AM - 21:30 AM</li>
+          </ul>
+          <h3>Pedestrian Bendung Tirtonadi Kota Surakarta</h3>
+          <div class="conference-map-content">
+            <p>Festival Bendung Tirtonadi berlangsung pada tanggal 17 sampai 18 Desember 2022 di Pedestrian Bendung Tirtonadi. Simpan tanggalnya dan jangan lupa datang yaa !</p>
+          </div>
+        </div>
+        <div class="col-lg-7 conference-map-item">
+          <span>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3955.2206954006133!2d110.81670061229661!3d-7.550896974525314!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a1699d711132d%3A0x7a20ae0e58bfd767!2sBendung%20Karet%20Tirtonadi!5e0!3m2!1sid!2sid!4v1670488408500!5m2!1sid!2sid" width="600" height="450" style="border: 0" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </span>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- ============= CONFERENCE-MAP END ============= -->
+
+  <!--==================== FOOTER ====================-->
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="footer-cont col-12 col-sm-6 col-lg-4">
+          <a href="index.html" class="logo"><img src="assets/img/logo-bt.svg" alt="logo" /></a>
+          <p>Festival Bendung Tirtonadi 2022</p>
+
+          <div class="footer-copyright"><a target="_blank" href="https://rovadex.com">BBWS Bengawan Solo</a> © 2022. All Rights Reserved.</div>
+        </div>
+        <div class="footer-item-link col-12 col-sm-6 col-lg-6">
+          <div class="footer-link">
+            <h5>Menu</h5>
+            <ul class="footer-list">
+              <li><a href="#">Home</a></li>
+              <li><a href="#about">Tema</a></li>
+              <li><a href="#schedule">Bintang Tamu</a></li>
+              <li><a href="#news">Lomba</a></li>
+              <li><a href="#location">Lokasi</a></li>
+            </ul>
+          </div>
+          <div class="footer-link">
+            <h5>Media Sosial</h5>
+            <ul class="footer-list">
+              <li><a target="_blank" href="https://www.facebook.com/bbwsbsolo">Facebook</a></li>
+              <li><a target="_blank" href="https://twitter.com/bbwsbsolo">Twitter</a></li>
+              <li><a target="_blank" href="https://www.instagram.com/pupr_sda_bengawansolo/">Instagram</a></li>
+              <li><a target="_blank" href="https://www.youtube.com/@BBWSBengawanSolo">Youtube</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!--================== FOOTER END ==================-->
+
+  <!--=================== TO TOP ===================-->
+  <a class="to-top" href="#home">
+    <i class="fa fa-angle-double-up" aria-hidden="true"></i>
+  </a>
+  <!--================= TO TOP END =================-->
+
+  <!--=================== SCRIPT	===================-->
+  <script src="assets/js/jquery-2.2.4.min.js"></script>
+  <script src="assets/js/slick.min.js"></script>
+  <script src="assets/js/rx-lazy.js"></script>
+  <script src="assets/js/jquery.nice-select.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=you_googlemap_key"></script>
+  <script src="assets/js/parallax.min.js"></script>
+  <script src="assets/js/jquery.fancybox.js"></script>
+  <script src="assets/js/scripts.js"></script>
+</body>
+
+</html>
